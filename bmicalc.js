6ca1fc443669c089c77b3bae2c33d6d154561d86
SamuelAjigbotosho <samajigbotosho@gmail.com>
2025-01-18 12:28:42 +0100

    Initial commit

--- bmicalc.js
+++ bmicalc.js
@@ -22,6 +22,7 @@ bmiBtn.addEventListener("click", (e) => {
   if (weight.value == 0 || height.value == "") {
     bmiResult.textContent = `Please fill out the input fields with valid figures.`;
     bmiResult.style.color = "red";
+    bmiResult.style.backgroundColor = "lightgray";
   } else if (BMI < 18.5) {
     bmiResult.textContent = `Your BMI is ${BMI} kg per metre square. You are underweight.`;
   } else if (BMI > 18.5 && BMI <= 24.9) {
@@ -38,4 +39,5 @@ resetBtn.addEventListener("click", (e) => {
   weight.value = "";
   height.value = "";
   bmiResult.textContent = "";
+  bmiResult.style.backgroundColor = "white";
 });
